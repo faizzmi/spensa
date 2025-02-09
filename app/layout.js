dbb665112b19395c346c4876abbd622d41700132
Faiz <83203236+faiz@users.noreply.github.com>
2025-02-09 23:22:07 +0800

    up

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Spensa",
-  description: "Minimalist spending tracker",
+  description: "minimalist spending tracker",
 };
 
 export default function RootLayout({ children }) {
